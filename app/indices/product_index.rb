408ef1b139ea0c3abca6dbb6c6d331d039d14ced
Iqra Rastee <iqra.rastee@arkhitech.com>
2014-10-28 19:19:49 +0500

    added facet to taxon_id

--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,12 +1,15 @@
 ThinkingSphinx::Index.define 'spree/product', with: :active_record do
   indexes :name, sortable: true
   indexes master.sku
+  indexes variants.sku, as: :variant_skus
   indexes :description
   indexes :meta_description
   indexes :meta_keywords
 
-  indexes taxons.name, as: :taxon, facet: true
-  has taxons.id, as: :taxon_ids
+  indexes taxons.name, as: :taxon_name
+  has taxons.id, as: :taxon_ids, facet: true  
+#  has variant.price , as: :price
+#  has variant.original_price , as: :original_price
 
   group_by :available_on
 end
