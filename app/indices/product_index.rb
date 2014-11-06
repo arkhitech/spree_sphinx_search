287c035fd4595f69dabb2a6206b949d834a4e326
Iqra Rastee <iqra.rastee@arkhitech.com>
2014-11-06 13:23:02 +0500

    linked filters to search

--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,15 +1,60 @@
 ThinkingSphinx::Index.define 'spree/product', with: :active_record do
-  indexes :name, sortable: true
-  indexes master.sku
-  indexes variants.sku, as: :variant_skus
-  indexes :description
-  indexes :meta_description
-  indexes :meta_keywords
+    #is_active_sql = "(spree_products.deleted_at IS NULL AND spree_products.available_on <= NOW() #{'AND (spree_products.count_on_hand > 0)' unless Spree::Config[:allow_backorders]} )"
+    is_active_sql = "(spree_products.deleted_at IS NULL AND spree_products.available_on <= NOW())"
+    option_sql = lambda do |option_name|
+      sql = <<-eos
+        SELECT DISTINCT p.id, ov.id
+        FROM #{Spree::OptionValue.table_name} AS ov
+        LEFT JOIN #{Spree::OptionType.table_name} AS ot ON (ov.option_type_id = ot.id)
+        LEFT JOIN spree_option_values_variants AS ovv ON (ovv.option_value_id = ov.id)
+        LEFT JOIN #{Spree::Variant.table_name} AS v ON (ovv.variant_id = v.id)
+        LEFT JOIN #{Spree::Product.table_name} AS p ON (v.product_id = p.id)
+        WHERE (ot.name = '#{option_name}' AND p.id>=$start AND p.id<=$end);
+        #{source.to_sql_query_range}
+      eos
+      sql.gsub("\n", ' ').gsub('  ', '')
+    end
 
-  indexes taxons.name, as: :taxon_name
-  has taxons.id, as: :taxon_ids, facet: true  
-#  has variant.price , as: :price
+    property_sql = lambda do |property_name|
+      sql = <<-eos
+          (SELECT spp.value
+          FROM #{Spree::ProductProperty.table_name} AS spp
+          INNER JOIN #{Spree::Property.table_name} AS sp ON sp.id = spp.property_id
+          WHERE sp.name = '#{property_name}' AND spp.product_id = #{Spree::Product.table_name}.id)
+      eos
+      sql.gsub("\n", ' ').gsub('  ', '')
+    end
+    
+    indexes :name, sortable: true
+    indexes master.sku
+    indexes variants.sku, as: :variant_skus
+    indexes :description
+    indexes :meta_description
+    indexes :meta_keywords
+
+    indexes taxons.name, as: :taxon_name
+    
+    has taxons.id, as: :taxon_ids, facet: true  
+    has taxons.id, as: :filter_taxon_ids, facet: true  
+  #  has variant.price , as: :price
 #  has variant.original_price , as: :original_price
 
-  group_by :available_on
-end
+    has master.default_price.amount, :as => :price
+
+    group_by "#{Spree::Product.table_name}.deleted_at"
+    group_by :available_on
+    has is_active_sql, :as => :is_active, :type => :boolean
+
+    #TODO need to define brand property
+    #has property_sql.call('brand'), as: :brand
+
+    source.model.indexed_attributes.each do |attr|
+      has attr[:field], attr[:options]
+    end
+    source.model.indexed_properties.each do |prop|
+      has property_sql.call(prop[:name].to_s), :as => :"#{prop[:name]}_property", :type => prop[:type]
+    end
+    source.model.indexed_options.each do |opt|
+      has option_sql.call(opt.to_s), :as => :"#{opt}_option", :source => :ranged_query, :type => :multi, :facet => true
+    end
+  end
--- lib/spree/search/thinking_sphinx.rb
+++ lib/spree/search/thinking_sphinx.rb
@@ -8,15 +8,49 @@ module Spree::Search
     # method should return AR::Relations with conditions {:conditions=> "..."} for Product model
     def get_products_conditions_for(base_scope, query)
       search_options = {page: page, per_page: per_page}
-      options = {}  #with_opts || 
+      options = {}     
+      cond_options = {}
+      if search
+        if search[:price_range_any].present?
+          case search[:price_range_any]
+          when /under.*10/i
+            unless Spree::Config.show_products_without_price
+              options.merge!(price: 0.001..10.0)
+            else
+              options.merge!(price: 0..10.0)
+            end          
+          when /10.*15/i
+            options.merge!(price: 10..15.0)
+          when /15.*18/i
+            options.merge!(price: 15..18.0)
+          when /18.*20/i
+            options.merge!(price: 18..20.0)
+          when /20.*over/i
+            options.merge!(price: 20..500000.0)
+          else
+            unless Spree::Config.show_products_without_price
+              options.merge!(price: 0.0001..500000.0)
+            end          
+          end
+        end
+        if search[:brand_any].present?
+          cond_options.merge!(taxon_name: search[:brand_any])
+        end        
+        if search[:taxons].present?
+          options.merge!(filter_taxon_ids: search[:taxons])
+        end
+      end
+      
       if taxon
         taxon_ids = taxon.self_and_descendants.map(&:id)
-        options.merge!(:taxon_ids => taxon_ids)
+        options.merge!(taxon_ids: taxon_ids)
       end
 
-      search_options.merge!(:with => options)
+      search_options.merge!(with: options)
+      search_options.merge!(conditions: cond_options)
 
       product_ids = Spree::Product.search_for_ids(query, search_options)
+      
       @properties[:product_ids] = product_ids
       @properties[:facets] = product_ids.facets
       base_scope.where("#{Spree::Product.table_name}.id" => @properties[:product_ids])
