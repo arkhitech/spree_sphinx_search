15019399b2baa1d4efeb80aaa95d7e504d6c8e33
Hisham Malik <hisham@arkhitech.com>
2016-05-22 14:52:14 +0500

    typo fixed

--- app/indices/shop_index.rb
+++ app/indices/shop_index.rb
@@ -1,6 +1,6 @@
 ThinkingSphinx::Index.define('spree/howmuch_shop', with: :active_record, delta: ThinkingSphinx::Deltas::SidekiqDelta) do
     
-    is_active_sql = "(#{Spree::HowmuchShop.table_name}.deleted_at IS NULL AND #{Spree::HowmuchShop.table_name}.is_authentic = 't'"
+    is_active_sql = "#{Spree::HowmuchShop.table_name}.deleted_at IS NULL AND #{Spree::HowmuchShop.table_name}.is_authentic = 't'"
     indexes :name, sortable: true
     indexes :address
     indexes :description
