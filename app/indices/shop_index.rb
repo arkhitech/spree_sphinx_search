7db2566ca5cc0bb473b80a3c4f2b608a05f472fe
Hisham Malik <hisham@arkhitech.com>
2018-02-14 22:51:16 +0500

    enhanced shop search

--- app/indices/shop_index.rb
+++ app/indices/shop_index.rb
@@ -3,16 +3,24 @@ ThinkingSphinx::Index.define('spree/howmuch_shop', with: :active_record, delta: 
     is_active_sql = "(#{Spree::HowmuchShop.table_name}.deleted_at IS NULL AND #{Spree::HowmuchShop.table_name}.published_at IS NOT NULL)"
     indexes :name, sortable: true
     indexes :description
+    indexes address.phone, as: :phone
+    indexes address.alternative_phone, as: :alternative_phone
     
     indexes address.address1, as: :address1
     indexes address.address2, as: :address2
     
+  
     indexes address.city, as: :city_name
+    indexes address.zipcode, as: :zipcode
+    
+    indexes address.state_name, as: :state_name
     
     has longitude, latitude
       
     has 'published_at IS NOT NULL', as: :published, type: :boolean
     has 'approved_at IS NOT NULL', as: :approved, type: :boolean
     has is_active_sql, as: :is_active, type: :boolean
+    
+    has address.state_id, as: :state_id
     has address.country_id, as: :country_id
   end
