bf7202712cb1bfccdb9c5cd401cbe9a763be23df
Hisham Malik <hisham@arkhitech.com>
2019-05-30 15:05:43 +0500

    get approved shop indices only

--- app/indices/shop_index.rb
+++ app/indices/shop_index.rb
@@ -1,19 +1,19 @@
 ThinkingSphinx::Index.define('spree/howmuch_shop', with: :active_record, delta: ThinkingSphinx::Deltas::SidekiqDelta) do
     
-    is_active_sql = "(#{Spree::HowmuchShop.table_name}.deleted_at IS NULL AND #{Spree::HowmuchShop.table_name}.published_at IS NOT NULL)"
+    is_active_sql = "(#{Spree::HowmuchShop.table_name}.deleted_at IS NULL AND #{Spree::HowmuchShop.table_name}.published_at IS NOT NULL) AND #{Spree::HowmuchShop.table_name}.approved_at IS NOT NULL)"
     indexes :name, sortable: true
     indexes :description
-    indexes address.phone, as: :phone
-    indexes address.alternative_phone, as: :alternative_phone
+    indexes store.address.phone, as: :phone
+    indexes store.address.alternative_phone, as: :alternative_phone
     
-    indexes address.address1, as: :address1
-    indexes address.address2, as: :address2
+    indexes store.address.address1, as: :address1
+    indexes store.address.address2, as: :address2
     
   
-    indexes address.city, as: :city_name
-    indexes address.zipcode, as: :zipcode
+    indexes store.address.city, as: :city_name
+    indexes store.address.zipcode, as: :zipcode
     
-    indexes address.state_name, as: :state_name
+    indexes store.address.state_name, as: :state_name
     
     has longitude, latitude
       
@@ -21,6 +21,6 @@ ThinkingSphinx::Index.define('spree/howmuch_shop', with: :active_record, delta: 
     has 'approved_at IS NOT NULL', as: :approved, type: :boolean
     has is_active_sql, as: :is_active, type: :boolean
     
-    has address.state_id, as: :state_id
-    has address.country_id, as: :country_id
+    has store.address.state_id, as: :state_id
+    has store.address.country_id, as: :country_id
   end
